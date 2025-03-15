c31a47b0875b72aaa84b4e487700241cbc8b1076
DarkRaven <aleksandrdugaev@gmail.com>
2025-03-15 22:57:57 +0300

    Добавлены мок данные в mockUserProfile

--- FakeNFT/Scenes /Profile/ProfileController.swift
+++ FakeNFT/Scenes /Profile/ProfileController.swift
@@ -20,6 +20,13 @@ final class ProfileController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private let mockUserProfile = UserProfile(name: "Студентус Практикумус",
+                                          avatar: "mock_avatar",
+                                          description: "Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100+ NFT, и еще больше — на моём сайте. Открыт к коллаборациям.",
+                                          website: "https://practicum.yandex.ru/ios-developer",
+                                          nfts: ["68","69","71","72","73","74","75","76","77","78","79","80","81"],
+                                          likes: ["5","13","19","26","27","33","35","39","41","47","56","66"])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
