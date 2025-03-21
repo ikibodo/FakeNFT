207a434488d6e58fcf76cb444900745bc2b1dbdd
kibodo <kibodo@ya.ru>
2025-03-21 15:25:21 +0100

    feat: statistics screen with table and cell added

---
+++ FakeNFT/Scenes /Statistics/StatisticsCell.swift
@@ -0,0 +1,108 @@
+//
+//  Untitled.swift
+//  FakeNFT
+//
+//  Created by N L on 21.3.25..
+//
+import UIKit
+
+final class StatisticsCell: UITableViewCell {
+    static let identifier = "StatisticsCell"
+    
+    private lazy var ratingLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .segmentActive
+        label.text = "1"
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var backgroundLabel: UILabel = {
+        let label = UILabel()
+        label.layer.cornerRadius = 12
+        label.layer.masksToBounds = true
+        label.backgroundColor = .segmentInactive
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var userpickImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 14
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .segmentActive
+        label.text = "Alex"
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var ownedNFTsLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .segmentActive
+        label.text = "100"
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        label.textAlignment = .right
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        backgroundColor = .clear
+        addSubViews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubViews() {
+        contentView.addSubview(ratingLabel)
+        contentView.addSubview(backgroundLabel)
+        backgroundLabel.addSubview(userpickImageView)
+        backgroundLabel.addSubview(nameLabel)
+        backgroundLabel.addSubview(ownedNFTsLabel)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            ratingLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            ratingLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            ratingLabel.widthAnchor.constraint(equalToConstant: 27),
+            ratingLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            backgroundLabel.leadingAnchor.constraint(equalTo: ratingLabel.trailingAnchor, constant: 8),
+            backgroundLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            backgroundLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            backgroundLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8),
+            
+            userpickImageView.leadingAnchor.constraint(equalTo: backgroundLabel.leadingAnchor, constant: 16),
+            userpickImageView.centerYAnchor.constraint(equalTo: backgroundLabel.centerYAnchor),
+            userpickImageView.widthAnchor.constraint(equalToConstant: 28),
+            userpickImageView.heightAnchor.constraint(equalToConstant: 28),
+            
+            nameLabel.leadingAnchor.constraint(equalTo: userpickImageView.trailingAnchor, constant: 8),
+            nameLabel.centerYAnchor.constraint(equalTo: backgroundLabel.centerYAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: ownedNFTsLabel.leadingAnchor, constant: -16),
+            
+            ownedNFTsLabel.trailingAnchor.constraint(equalTo: backgroundLabel.trailingAnchor, constant: -16),
+            ownedNFTsLabel.centerYAnchor.constraint(equalTo: backgroundLabel.centerYAnchor),
+            ownedNFTsLabel.heightAnchor.constraint(equalToConstant: 28)
+        ])
+    }
+    
+    func configure() {
+    }
+}
---
+++ FakeNFT/Scenes /Statistics/StatisticsViewController.swift
@@ -0,0 +1,91 @@
+//
+//  StatisticsViewController.swift
+//  FakeNFT
+//
+//  Created by N L on 20.3.25..
+//
+import UIKit
+
+final class StatisticsViewController: UIViewController {
+    private let servicesAssembly: ServicesAssembly
+    
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(named: "Sort"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(didSortButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none
+        tableView.tableHeaderView = UIView(frame: .zero)
+        tableView.register(StatisticsCell.self, forCellReuseIdentifier: StatisticsCell.identifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    init(servicesAssembly: ServicesAssembly) {
+        self.servicesAssembly = servicesAssembly
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        addSubViews()
+        addConstraints()
+        setupNavigationBar()
+    }
+    
+    private func addSubViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        guard (navigationController?.navigationBar) != nil else { return }
+        
+        let buttonContainer = UIView(frame: CGRect(x: 0, y: 0, width: 42, height: 42))
+        buttonContainer.addSubview(button)
+        button.frame = buttonContainer.bounds
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: button)
+    }
+    
+    @objc private func didSortButtonTapped() {
+        print("Нажата кнопка SortButton")
+    }
+}
+
+extension StatisticsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: StatisticsCell.identifier, for: indexPath) as? StatisticsCell else { return UITableViewCell() }
+        cell.configure()
+               return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        88
+    }
+}
