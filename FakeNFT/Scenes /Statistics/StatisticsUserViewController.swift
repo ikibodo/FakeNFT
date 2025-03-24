91fbb8c04e64203b2148f9cd343833b5fb950c4d
kibodo <kibodo@ya.ru>
2025-03-24 16:06:35 +0100

    feat: display of user image, name and description added

--- FakeNFT/Scenes /Statistics/StatisticsUserViewController.swift
+++ FakeNFT/Scenes /Statistics/StatisticsUserViewController.swift
@@ -5,9 +5,10 @@
 //  Created by N L on 22.3.25..
 //
 import UIKit
+import Kingfisher
 
 final class StatisticsUserViewController: UIViewController, StatisticsUserViewProtocol {
-    private var presenter: StatisticsUserPresenter?
+    private let presenter: StatisticsUserPresenter?
     
     private lazy var button: UIButton = {
         let button = UIButton(type: .custom)
@@ -15,6 +16,44 @@ final class StatisticsUserViewController: UIViewController, StatisticsUserViewPr
         button.tintColor = .segmentActive
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         return button
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 35
+        imageView.image = UIImage(systemName: "person.crop.circle.fill")
+        imageView.tintColor = .yaGrayUniversal
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .segmentActive
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .segmentActive
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     init(presenter: StatisticsUserPresenter) {
@@ -31,11 +70,23 @@ final class StatisticsUserViewController: UIViewController, StatisticsUserViewPr
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupNavigationBar()
+        addSubViews()
+        addConstraints()
         presenter?.loadUserData()
     }
     
     func updateUserInfo(_ user: StatisticsUser) {
-        // TODO
+        nameLabel.text = user.name
+        descriptionLabel.text = user.description
+        if let url = URL(string: user.avatar ?? "") {
+            avatarImageView.kf.setImage(
+                with: url,
+                placeholder: UIImage(systemName: "person.crop.circle.fill"),
+                options: [ .cacheOriginalImage ]
+            )
+        } else {
+            avatarImageView.image = UIImage(systemName: "person.crop.circle.fill")
+        }
     }
     
     private func setupNavigationBar() {
@@ -46,6 +97,35 @@ final class StatisticsUserViewController: UIViewController, StatisticsUserViewPr
         buttonContainer.addSubview(button)
         button.frame = buttonContainer.bounds
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: button)
+    }
+    
+    private func addSubViews() {
+        view.addSubview(stackView)
+        stackView.addSubview(avatarImageView)
+        stackView.addSubview(nameLabel)
+        view.addSubview(descriptionLabel)
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            stackView.heightAnchor.constraint(equalToConstant: 70),
+            
+            avatarImageView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            avatarImageView.topAnchor.constraint(equalTo: stackView.topAnchor),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 70),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 70),
+            
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            nameLabel.centerYAnchor.constraint(equalTo: stackView.centerYAnchor),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 20),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
     
     @objc private func didTapButton() {
