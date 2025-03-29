6c8c5ba07d8c14bef9df33abe9827e08934d8e26
DarkRaven <aleksandrdugaev@gmail.com>
2025-03-29 22:55:30 +0300

    Экран "Избранные NFT" на мок данных

---
+++ FakeNFT/Scenes /Profile/FavoriteNFT/FavoriteNFTCell.swift
@@ -0,0 +1,185 @@
+//
+//  FavoriteNFTCell.swift
+//  FakeNFT
+//
+//  Created by Aleksandr Dugaev on 27.03.2025.
+//
+
+import UIKit
+import Kingfisher
+
+final class FavoriteNFTCell: UICollectionViewCell, ReuseIdentifying {
+    
+    // MARK: - Private Properties
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 12
+        stackView.alignment = .center
+        stackView.addArrangedSubview(nftContainerView)
+        stackView.addArrangedSubview(mainContentStackView)
+        return stackView
+    }()
+    
+    private lazy var nftContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nftImageView)
+        view.addSubview(heartButton)
+        
+        NSLayoutConstraint.activate([
+            nftImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            nftImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            nftImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            nftImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            heartButton.topAnchor.constraint(equalTo: view.topAnchor, constant: -6.19),
+            heartButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 6.19),
+            heartButton.widthAnchor.constraint(equalToConstant: 42),
+            heartButton.heightAnchor.constraint(equalToConstant: 42)
+        ])
+        
+        return view
+    }()
+    
+    private lazy var nftImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 12
+        imageView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var heartButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "like"), for: .normal)
+        button.tintColor = UIColor.white
+        button.addTarget(self, action: #selector(heartButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var mainContentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private lazy var nameAndRatingStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 4
+        return stackView
+    }()
+    
+    private lazy var nftNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var ratingStarsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 2
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var nftPriceLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var priceStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.spacing = 4
+        return stackView
+    }()
+    
+    private var starImageViews: [UIImageView] = []
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(mainStackView)
+        
+        mainContentStackView.addArrangedSubview(nameAndRatingStackView)
+        mainContentStackView.addArrangedSubview(priceStackView)
+        
+        nameAndRatingStackView.addArrangedSubview(nftNameLabel)
+        nameAndRatingStackView.addArrangedSubview(ratingStarsStackView)
+        
+        priceStackView.addArrangedSubview(nftPriceLabel)
+        
+        for _ in 0..<5 {
+            let starImageView = UIImageView()
+            starImageView.contentMode = .scaleAspectFit
+            ratingStarsStackView.addArrangedSubview(starImageView)
+            starImageViews.append(starImageView)
+            
+            starImageView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                starImageView.heightAnchor.constraint(equalToConstant: 12),
+                starImageView.widthAnchor.constraint(equalToConstant: 12)
+            ])
+        }
+        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            ratingStarsStackView.heightAnchor.constraint(equalToConstant: 12),
+        ])
+    }
+    
+    func configure(with nft: Nft) {
+        nftNameLabel.text = nft.name
+        nftPriceLabel.text = "\(nft.price) ETH"
+        nftImageView.kf.setImage(with: nft.images[0], placeholder: UIImage(systemName: "photo"))
+        
+        let clampedRating = max(0, min(nft.rating, 5))
+        
+        for (index, star) in starImageViews.enumerated() {
+            star.image = UIImage(systemName: "star.fill")
+            star.tintColor = index < clampedRating ? .systemYellow : .lightGray
+        }
+    }
+    
+    // MARK: - Actions
+    @objc private func heartButtonTapped() {
+        heartButton.tintColor =  (heartButton.tintColor == UIColor.white) ? UIColor.red : UIColor.white
+    }
+}
+
--- FakeNFT/Scenes /Profile/FavoriteNFT/FavoritesNFTController.swift
+++ FakeNFT/Scenes /Profile/FavoriteNFT/FavoritesNFTController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 protocol FavoritesNFTControllerProtocol: AnyObject {
-
+    func updateEmptyState(isHidden: Bool)
 }
 
 final class FavoritesNFTController: UIViewController {
@@ -24,7 +24,35 @@ final class FavoritesNFTController: UIViewController {
         return button
     }()
     
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: (view.frame.width - 8 - 16 * 2) / 2, height: 80)
+        layout.minimumInteritemSpacing = 8
+        layout.minimumLineSpacing = 20
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 16, bottom: 20, right: 16)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.register(FavoriteNFTCell.self)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private lazy var emptyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "У вас ещё нет избранных NFT"
+        label.textAlignment = .center
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.isHidden = true
+        return label
+    }()
+    
     private var presenter: FavoritesNFTPresenterProtocol?
+    private var favoriteNFTs: [Nft] = []
     
     // MARK: - Public Methods
     func setPresenter(_ presenter: FavoritesNFTPresenterProtocol) {
@@ -38,11 +66,61 @@ final class FavoritesNFTController: UIViewController {
         navigationItem.title = NSLocalizedString("Profile.FavoritesNFT.title", comment: "Избранные NFT")
         navigationItem.leftBarButtonItem = backButton
         
+        setupCollectionView()
+        loadNFTs()
+        
+        updateEmptyState(isHidden: !favoriteNFTs.isEmpty)
+    }
+    
+    private func setupCollectionView() {
+        view.addSubview(collectionView)
+        view.addSubview(emptyLabel)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            emptyLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            emptyLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func loadNFTs() {
+        
+        favoriteNFTs = presenter?.loadNFTs() ?? []
+        collectionView.reloadData()
+    }
+    
+    func updateEmptyState(isHidden: Bool) {
+        emptyLabel.isHidden = isHidden
+        collectionView.isHidden = !isHidden
     }
     
     // MARK: - Actions
     @objc private func modalCloseButtonTapped() {
         self.navigationController?.popViewController(animated: true)
+    }
+}
+
+extension FavoritesNFTController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return favoriteNFTs.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: FavoriteNFTCell = collectionView.dequeueReusableCell(indexPath: indexPath)
+        cell.configure(with: favoriteNFTs[indexPath.item])
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension FavoritesNFTController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedNFT = favoriteNFTs[indexPath.item]
+        print("Выбрана NFT: \(selectedNFT.name), автор: \(selectedNFT.author), цена: $\(selectedNFT.price)")
     }
 }
 
